f3362677e5a2fc2ada5f809dcfad139f851c47f7
tebaly <support@freedomsex.net>
2020-06-17 20:37:29 +0300

    first commit

---
+++ index.vue
@@ -0,0 +1,160 @@
+<script>
+import hasher from '@freedomsex/simple-hash';
+
+export default {
+  props: [],
+  data() {
+    return {
+      id: null,
+      token: null,
+      image: null,
+      time: null,
+      expires: null,
+      process: false,
+      updated: null,
+      timer: null,
+      code: '',
+      inc: 0,
+    };
+  },
+  mounted() {
+    this.start();
+  },
+  methods: {
+    now() {
+      return String(new Date().getTime()).slice(0, -3);
+    },
+    freze() {
+      return (this.now() - this.updated) < 3;
+    },
+    expired() {
+      return this.now() > this.expires;
+    },
+    close() {
+      this.$emit('close');
+    },
+
+    wait(time) {
+      this.time = time;
+      this.timer = setTimeout(this.getToken, time * 1000);
+      this.updated = this.now();
+    },
+
+    start() {
+      this.process = true;
+      this.$api.res('time', 'verify').load({hash: hasher.random()}).then(({data}) => {
+        this.id = data.id;
+        this.wait(data.time);
+      }).catch(() => {
+        this.process = false;
+      });
+    },
+    getToken() {
+      this.$api.res('token', 'verify').post({id: this.id}).then(({data}) => {
+        if (data.token) {
+          this.token = data.token;
+          this.expires = data.token.split('.', 3)[1];
+          this.$emit('token', this.token);
+          this.draw();
+        } else {
+          this.wait(data.retry);
+        }
+      }).catch(() => {
+        this.process = false;
+      });
+    },
+
+
+    draw() {
+      this.$api.res(`image/${this.token}`, 'verify').load().then(({data}) => {
+        this.process = false;
+        this.image = `data:image/jpeg;base64,${data.image}`;
+        this.updated = this.now();
+        this.inc += 1;
+      }).catch(() => {
+        this.process = false;
+      });
+    },
+    update() {
+      this.image = null;
+      this.start();
+    },
+    refresh() {
+      if (!this.process && this.expired()) {
+        this.update();
+      }
+    },
+    renew() {
+      if (!this.process && !this.freze()) {
+        this.update();
+      }
+    },
+    input() {
+      this.$emit('input', this.code);
+    },
+  },
+};
+</script>
+
+<template>
+  <div class="simple-captcha"  style="max-width: 270px;">
+    <div class="activity-section__tile">
+      <div class="image-field" @click="renew()">
+        <div class="image-field__addon">
+          <span aria-hidden="true" class="glyphicon glyphicon-arrow-down"></span>
+        </div>
+        <div class="image-field__image">
+          <img :src="image" v-if="image"
+            width="100" height="50">
+          <span class="image-field__loader" v-else>•••</span>
+        </div>
+      </div>
+    </div>
+    <div class="activity-section__tile">
+      <div class="form-group">
+        <input class="form-control" type="tel"
+         placeholder="Введите код"
+         inputmode="numeric" autocomplete="off"
+         v-model="code"
+         @change="input">
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="less">
+.simple-captcha {
+  .image-field {
+    border: 1px solid @gray-light;
+    border-radius: 4px;
+    display: flex;
+    align-items: stretch;
+    min-height: 55px;
+    &__addon {
+      display: flex;
+      align-items: center;
+      padding: @indent-sm;
+      background: @smoke;
+      span {
+        color: @carbon;
+        font-size: 18px;
+      }
+    }
+    &__image {
+      flex: 1 0 auto;
+      display: flex;
+      align-items: center;
+      justify-content: space-around;
+      img {
+        margin: 0;
+      }
+    }
+    &__loader {
+      color: @gray-light;
+      font-size: 38px;
+      line-height: 1;
+    }
+  }
+
+}
+</style>
